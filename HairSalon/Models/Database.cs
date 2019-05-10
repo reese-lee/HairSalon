6123eeb4d18549676adc849d187d17045f264767
Reese Lee <reese.lee.cy@gmail.com>
2019-05-10 08:27:59 -0700

    add initial files, with successful dotnet build to add bin and obj folders and files

---
+++ HairSalon/Models/Database.cs
@@ -0,0 +1,19 @@
+using System;
+using MySql.Data.MySqlClient;
+using HairSalon;
+
+namespace HairSalon.Models
+{
+
+    public class DB
+    {
+
+      public static MySqlConnection Connection()
+      {
+          MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString);
+          return conn;
+      }
+
+    }
+
+}
