5605389c4605b5eb0626fb05145d60fc6fe24f8e
Reese Lee <reese.lee.cy@gmail.com>
2019-05-10 09:34:07 -0700

    add remaining empty files and folders, including some renaming to follow convention

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using BestRestaurant.Models;
+using System.Collections.Generic;
+using System;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controller
+  {
+
+    [HttpGet("/stylists")]
+    public ActionResult Index()
+    {
+      List<Stylists> allStylists = Stylists.GetAll();
+      return View(allStylists);
+    }
+
+    [HttpGet("/stylists/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+
+    [HttpPost("/stylists")]
+    public ActionResult Create(string type)
+    {
+      Stylists myStylists = new Stylists(type);
+      myStylists.Save();
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/stylists/{id}/restaurant/new")]
+    public ActionResult Show()
+    {
+      // Restaurant newRestaurant = new Restaurant(name, address, phoneNumber, stylistsId);
+      List<Stylists> allStylists = Stylists.GetAll();
+      return View();
+    }
+
+    // [HttpPost("/stylists/{id}/restaurant")]
+    // public ActionResult New(string name, string address, string phoneNumber, int stylistsId)
+    // {
+    //   Restaurant myRestaurant = new Restaurant(name, address, phoneNumber, stylistsId);
+    //   return RedirectToAction("Index");
+    // }
+
+    // [HttpGet("/animals/SortByType")]
+    // public ActionResult SortByType()
+    // {
+    //   // Animal newAnimal = new Animal();
+    //   List<Animal> allSortedAnimals = Animal.SortByType();
+    //   return View(allSortedAnimals);
+    // }
+  }
+}
